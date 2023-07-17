93b7d771bc76218357aa160eed29f72d2c50f9da
Nonnok <donntenn0917@icloud.com>
2023-07-17 20:48:05 +0900

    Fix JavaScript

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ let nav = document.querySelector("#navArea");
 let btn = document.querySelector(".toggle-btn");
 let mask = document.querySelector("#mask");
 
-btn.onclick = () => {
-  nav.classList.toggle("open");
-}
-
-mask.onclick = () => {
-  nav.classList.toggle("open");
-}
-
 // buy button alert
 const buyButton = document.getElementById('buy');
 buyButton.addEventListener('click', () => {
   alert('架空のサイトであるため購入できません。');
 });
+
+// btn.onclick = () => {
+//   nav.classList.toggle("open");
+// }
+
+// mask.onclick = () => {
+//   nav.classList.toggle("open");
+// }
